26da9b60651050e1607e2115e44f416816b5619d
Joan Nyawira Kori <korijoan171@gmail.com>
2025-03-13 12:43:37 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// Define the function findMatching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Define the function fuzzyMatch
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Define the function matchName
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
